88efd121f8ff05a1805ef1db3ea9fb7832414896
locusttoybox <locusttoybox@gmail.com>
2025-04-25 17:31:26 +0300

    aboutProject data file

---
+++ src/app/data/aboutProject.ts
@@ -0,0 +1,28 @@
+export interface AboutProject {
+  title: string;
+  description: string;
+}
+
+export const aboutProject: AboutProject[] = [
+  {
+    title: 'Why ANT',
+    description:
+      'Inspired by Nature Like an ant colony, our platform is built for resilience, collaboration, and efficiency. Built for Flexibility From small households to large-scale solar farms—ANT scales with your needs. Future-Proof and Sustainable Accelerate your transition to a green, decentralized energy future',
+  },
+];
+
+export const aboutProjectPoints = [
+  {
+    title: 'Data Security & Privacy',
+    description: 'Compliant with GDPR and energy sector standards',
+  },
+  {
+    title: 'APIs for Third-Party Integration',
+    description:
+      'Open interfaces for connection with smart inverters, EV chargers, grid operators, and IoT devices',
+  },
+  {
+    title: 'Cloud-Based Infrastructure',
+    description: 'Secure, scalable, and accessible from any device',
+  },
+];
