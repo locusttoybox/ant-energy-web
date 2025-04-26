85a4289e46e3e0ace8ef0b441ecf5f7fd42e85f4
locusttoybox <locusttoybox@gmail.com>
2025-04-26 10:21:07 +0300

    `Refactor AboutProject component: update class names and media queries`

--- src/app/components/AboutProject.tsx
+++ src/app/components/AboutProject.tsx
@@ -7,7 +7,7 @@ export default function AboutProject() {
     <section id="about-project" className="bg-foreground flex h-full w-screen px-10 py-24 lg:px-36">
       <div className="w-full">
         {/* Titles Section */}
-        <div className="flex flex-col justify-between gap-8 sm:flex-row lg:pb-12">
+        <div className="flex flex-col justify-between gap-8 sm:flex-row md:pb-12 lg:pb-12">
           {aboutProject.map(({ title }, idx) => (
             <div key={idx} className="mb-10 flex-1 sm:mb-0">
               <h3 className="text-accent text-4xl font-semibold">{title}</h3>
@@ -17,9 +17,9 @@ export default function AboutProject() {
 
         {/* Content Section */}
         <div className="w-full">
-          <div className="flex flex-col gap-12 lg:flex-row">
+          <div className="flex flex-col gap-12 md:flex-row lg:flex-row">
             {/* Descriptions */}
-            <div className="flex w-full flex-col gap-8 lg:w-full">
+            <div className="flex w-full flex-col gap-8 md:w-full lg:w-full">
               {aboutProject.map(({ description }, idx) => (
                 <div key={idx} className="w-full">
                   <p className="text-secondary text-3xl">{description}</p>
@@ -28,7 +28,7 @@ export default function AboutProject() {
             </div>
 
             {/* Points */}
-            <div className="flex flex-col gap-6 lg:w-1/2">
+            <div className="flex flex-col gap-6 md:w-1/2 lg:w-1/2">
               {aboutProjectPoints.map(({ title, description }, idx) => (
                 <div key={idx} className="flex flex-col gap-2">
                   <h3 className="text-accent text-xl font-semibold">{title}</h3>
