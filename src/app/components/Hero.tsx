9b6eec73da8398bf61942bcacf335aeaeaacb802
locusttoybox <locusttoybox@gmail.com>
2025-04-26 14:09:51 +0300

    `Refactor Hero component layout and styling`

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -15,14 +15,20 @@ export default function Hero() {
       <Image src={HeroImage.src} alt="ant" layout="fill" objectFit="cover" className="z-0" />
 
       {/* Hero Content */}
-      <div className="absolute inset-0 z-10 mx-auto flex flex-col items-center justify-center px-6 text-center lg:flex-row lg:justify-center lg:text-left">
-        <div className="lg:w-4/6">
-          <h1 className="text-foreground mb-10 text-4xl font-extrabold md:text-5xl lg:text-6xl">
-            ANT Energy Automated Network for Transmission Inspired by nature, powered by innovation.
-          </h1>
+      <div className="w-full">
+        <div className="absolute inset-0 z-10 mx-auto flex w-screen flex-col items-center justify-center px-6 py-20 sm:px-6 md:py-24 lg:flex-row lg:justify-center lg:px-36 lg:py-24 lg:text-left">
+          <div className="lg:flex-col-2 flex">
+            <div className="lg:w-2/3">
+              <h1 className="text-foreground mb-10 text-4xl font-extrabold md:text-5xl lg:text-6xl">
+                ANT Energy Automated Network for Transmission Inspired by nature, powered by
+                innovation.
+              </h1>
 
-          <div className="flex justify-center gap-4 lg:justify-start">
-            <Button variant="outlineBlack">Explore the Platform</Button>
+              <div className="flex justify-center gap-4 lg:justify-start">
+                <Button variant="outlineBlack">Explore the Platform</Button>
+              </div>
+            </div>
+            <div className="lg:w-1/3"></div>
           </div>
         </div>
       </div>
